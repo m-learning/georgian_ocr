aba0cee81f61127f5ee9f87ef8c0866d217c09e5
sosoxuc <soso_xuc@yahoo.com>
2017-03-18 17:46:12 +0400

    upgrade to keras2

--- src/trainer.py
+++ src/trainer.py
@@ -45,9 +45,10 @@ from scipy import ndimage
 import matplotlib.image as mpimg
 import pylab
 from keras import backend as K
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers import Input, Dense, Activation
-from keras.layers import Reshape, Lambda, merge
+from keras.layers import Reshape, Lambda
+from keras.layers.merge import add, concatenate
 from keras.models import Model
 from keras.layers.recurrent import GRU
 from keras.optimizers import SGD
@@ -161,7 +162,7 @@ def paint_text(text, w, h, rotate=False, ud=False, multi_fonts=False):
 	if rotate:
 		a = image.random_rotation(a, 3 * (w - top_left_x) / w + 1)
 	a = speckle(a)
-	# mpimg.imsave(text + ".png",a[0])
+	mpimg.imsave(text + ".png",a[0])
 	return a
 
 
@@ -478,11 +479,11 @@ def train(run_name, start_epoch, stop_epoch, img_w):
 								 )
 	act = 'relu'
 	input_data = Input(name='the_input', shape=input_shape, dtype='float32')
-	inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-						  activation=act, init='he_normal', name='conv1')(input_data)
+	inner = Conv2D(conv_num_filters, (filter_size, filter_size), padding='same',
+						  activation=act, kernel_initializer='he_normal', name='conv1')(input_data)
 	inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
-	inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-						  activation=act, init='he_normal', name='conv2')(inner)
+	inner = Conv2D(conv_num_filters, (filter_size, filter_size), padding='same',
+						  activation=act, kernel_initializer='he_normal', name='conv2')(inner)
 	inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
 	conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_num_filters)
@@ -493,17 +494,17 @@ def train(run_name, start_epoch, stop_epoch, img_w):
 
 	# Two layers of bidirecitonal GRUs
 	# GRU seems to work as well, if not better than LSTM:
-	gru_1 = GRU(rnn_size, return_sequences=True, init='he_normal', name='gru1')(inner)
-	gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='gru1_b')(inner)
-	gru1_merged = merge([gru_1, gru_1b], mode='sum')
-	gru_2 = GRU(rnn_size, return_sequences=True, init='he_normal', name='gru2')(gru1_merged)
-	gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='gru2_b')(gru1_merged)
+	gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
+	gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
+	gru1_merged = add([gru_1, gru_1b])
+	gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+	gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
 
 	# transforms RNN output to character activations:
-	inner = Dense(img_gen.get_output_size(), init='he_normal',
-				  name='dense2')(merge([gru_2, gru_2b], mode='concat'))
+	inner = Dense(img_gen.get_output_size(), kernel_initializer='he_normal',
+				  name='dense2')(concatenate([gru_2, gru_2b]))
 	y_pred = Activation('softmax', name='softmax')(inner)
-	Model(input=[input_data], output=y_pred).summary()
+	Model(inputs=[input_data], outputs=y_pred).summary()
 
 	labels = Input(name='the_labels', shape=[img_gen.absolute_max_string_len], dtype='float32')
 	input_length = Input(name='input_length', shape=[1], dtype='int64')
@@ -515,7 +516,7 @@ def train(run_name, start_epoch, stop_epoch, img_w):
 	# clipnorm seems to speeds up convergence
 	sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
 
-	model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
+	model = Model(inputs=[input_data, labels, input_length, label_length], outputs=[loss_out])
 
 	# the loss calc occurs elsewhere, so use a dummy lambda func for the loss
 	model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
@@ -527,8 +528,8 @@ def train(run_name, start_epoch, stop_epoch, img_w):
 
 	viz_cb = VizCallback(run_name, test_func, img_gen.next_val())
 
-	model.fit_generator(generator=img_gen.next_train(), samples_per_epoch=(words_per_epoch - val_words),
-						nb_epoch=stop_epoch, validation_data=img_gen.next_val(), nb_val_samples=val_words,
+	model.fit_generator(generator=img_gen.next_train(), steps_per_epoch=(words_per_epoch - val_words),
+						epochs=stop_epoch, validation_data=img_gen.next_val(), validation_steps=val_words,
 						callbacks=[viz_cb, img_gen], initial_epoch=start_epoch)
 
 if __name__ == '__main__':
